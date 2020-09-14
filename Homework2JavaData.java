3443d9bfca43f27a267317d4047f84bf47eb80a4
MikeCaufield <mc735@njit.edu>
2020-09-14 19:48:54 -0400

    Homework2JavaData

---
+++ Homework2JavaData.java
@@ -0,0 +1,11 @@
+public class Homework2JavaData{
+   public static void main(String[] args){
+      String name = "Mike Caufield";
+      System.out.println("Hello, my name is " + name + " . ");
+      int age = 21;
+      System.out.println("I have just recently turned " + age + " years old.");
+      float height = 5.11f;
+      float weight = 168.9f;
+      System.out.println("I am currently " + height + " feet tall and weight appoximately " + weight + " pounds.");
+   }
+}
